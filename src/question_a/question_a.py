38cf6143c4a5d805ec462d605f440f793811cc6d
ACCabris <alyssa.brister@g.austincc.edu>
2025-03-14 19:12:13 +0000

    midterm1

--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,20 @@
-#write functions here, don't add input('') statements here!
-def test_config():
-    return True
+def get_day_of_week(day):
+    if day < 1 or day > 7:
+        return "Invalid input. Please enter a number between 1 and 7."
+    elif day == 1:
+        return "Monday"
+    elif day == 2:
+        return "Tuesday"
+    elif day == 3:
+        return "Wednesday"
+    elif day == 4:
+        return "Thursday"
+    elif day == 5:
+        return "Friday"
+    elif day == 6:
+        return "Saturday"
+    else:
+        return "Sunday"
+
+day = int(input("Enter a number between 1 and 7: "))
+print(get_day_of_week(day))
--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,7 @@
-#write functions here, don't add input('') statements here!
+def get_fahrenheit(celsius):
+    return (9/5) * celsius + 32
+
+print("{:<10} {:<10}".format("Celsius", "Fahrenheit"))
+for c in range(0, 21):
+    f = get_fahrenheit(c)
+    print("{:<10} {:<10.1f}".format(c, f))
